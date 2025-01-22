f96de1a1f38c26b69b1f8756d7dfe27d04740a9c
hendisantika <hendisantika@yahoo.co.id>
2025-01-22 09:16:07 +0700

    Add generate in LlamaRestController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/aiollamasample/controller/LlamaRestController.java
+++ src/main/java/id/my/hendisantika/aiollamasample/controller/LlamaRestController.java
@@ -1,7 +1,12 @@
 package id.my.hendisantika.aiollamasample.controller;
 
+import id.my.hendisantika.aiollamasample.model.LlamaResponse;
 import id.my.hendisantika.aiollamasample.service.LlamaAiService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -20,4 +25,11 @@ public class LlamaRestController {
 
     private final LlamaAiService llamaAiService;
 
+    @GetMapping("api/v1/ai/generate")
+    public ResponseEntity<LlamaResponse> generate(
+            @RequestParam(value = "prompt", defaultValue = "Why is the sky blue?")
+            String prompt) {
+        final LlamaResponse aiResponse = llamaAiService.generateMessage(prompt);
+        return ResponseEntity.status(HttpStatus.OK).body(aiResponse);
+    }
 }
