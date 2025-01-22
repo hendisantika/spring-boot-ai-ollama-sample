a339b1a9872efae137c4d43f3246ef97c7d77dc1
hendisantika <hendisantika@yahoo.co.id>
2025-01-22 09:14:34 +0700

    Add LlamaAiService🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/aiollamasample/service/LlamaAiService.java
@@ -0,0 +1,21 @@
+package id.my.hendisantika.aiollamasample.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : ai-ollama-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 22/01/25
+ * Time: 09.14
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@RequiredArgsConstructor
+public class LlamaAiService {
+    private final OllamaChatModel ollamaChatModel;
+}
